b7f401574f82a5f3a5b22c050b089d7f8e6e6463
admin <admin@Skyscrapper>
2022-02-17 12:57:04 +0530

    Updated

--- api-gateway/src/main/java/com/jbk/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jbk/ApiGatewayApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
+		System.out.println("Make changes");
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
